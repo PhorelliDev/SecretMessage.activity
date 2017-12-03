6e4120db78f7b55f102626e1c180593b7333c55a
Thekiddiejsandpython <bustida@gmail.com>
2017-12-03 00:03:30 -0700

    Update activity.py

--- activity.py
+++ activity.py
@@ -1,21 +1,3 @@
-# Copyright 2009 Simon Schampijer
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
-"""HelloWorld Activity: A case study for developing an activity."""
-
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
@@ -29,15 +11,15 @@ from sugar3.activity.widgets import TitleEntry
 from sugar3.activity.widgets import StopButton
 from sugar3.activity.widgets import ShareButton
 from sugar3.activity.widgets import DescriptionItem
-from playsound import playsound
 
-class SugarSoundsActivity(activity.Activity):
-    """HelloWorldActivity class as specified in activity.info"""
+
+class SecretMessageActivity(activity.Activity):
+    
+   
 
     def __init__(self, handle):
-        """Set up the HelloWorld activity."""
+        
         activity.Activity.__init__(self, handle)
-
         # we do not have collaboration features
         # make the share option insensitive
         self.max_participants = 1
@@ -74,13 +56,24 @@ class SugarSoundsActivity(activity.Activity):
         self.set_toolbar_box(toolbar_box)
         toolbar_box.show()
 
-        # label with the text, make the string translatable
+       
 
         # Main layout setup
-        layout = Gtk.Grid(column_spacing=6, row_spacing=6)
-        label = Gtk.Label(_("Welcome to Sounds!"))
-        layout.add(label)
-        playsound(usr/share/sugar/activities/Sugar-Sounds.activity/res/demo.wav)
+        layout = Gtk.Grid()
+        self.msg = Gtk.Label("This is a secret message!")
+        sbutton = Gtk.Button("Show message")
+        sbutton.connect("clicked", self._show)
+        hbutton = Gtk.Button("Hide Message")
+        hbutton.connect("clicked", self._hide)
+        layout.attach(self.msg, 0, 0, 2, 1)
+        layout.attach_next_to(sbutton, self.msg, Gtk.PositionType.BOTTOM, 1, 1)
+        layout.attach_next_to(hbutton, sbutton, Gtk.PositionType.RIGHT, 1, 1)
         self.set_canvas(layout)
         self.show_all()
-        
+    # Show and hide secret message
+    def _show(self, button):
+        self.msg.set_text("This is a secret message!")
+
+    def _hide(self, button):
+        self.msg.set_text("")
+
